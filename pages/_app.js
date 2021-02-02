639985558b5c3d8b037be9f4ee7c5a103de7a326
Jenia <jeniabrook@gmail.com>
2021-02-02 19:56:52 +0200

    inject global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import "../styles/global.css";
+import { Global } from '@emotion/react';
+
+import { globalCss } from '../styles/global';
+import { Fragment } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Global styles={globalCss} />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
