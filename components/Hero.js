3164291b655e360a1d0bc2a58093a13bab7215bb
Jenia <jeniabrook@gmail.com>
2021-02-02 21:22:40 +0200

    Scaffolds

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,60 @@
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
+import { typography } from '../styles/theme';
+
+const blink = keyframes`
+  0% { opacity: 1.0; }
+  50% { opacity: 0.0; }
+  100% { opacity: 1.0; }
+`;
 
 const heroCss = css`
   min-height: 100vh;
   background-color: #f66;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const introductionCss = css`
+  font-family: ${typography.fontSerif};
+  font-size: 4rem;
+
+  @media (max-width: 1024px) {
+    font-size: 2.5rem;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 2.2rem;
+  }
+`;
+
+const headingTitleCss = css`
+  font-size: 9rem;
+  font-weight: 800;
+  color: #ffffff;
+  margin-top: 30px;
+  text-align: center;
+
+  ::after {
+    content: '|';
+    animation: ${blink} 1s ease infinite;
+  }
+
+  @media (max-width: 1024px) {
+    font-size: 7rem;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 4rem;
+  }
 `;
 
 function Hero() {
   return (
     <section css={heroCss}>
-      <div>Hi</div>
+      <div css={introductionCss}>I'm Jenia Brook.</div>
+      <h1 css={headingTitleCss}>Front-end engineer.</h1>
     </section>
   );
 }
--- styles/fontFaces.js
+++ styles/fontFaces.js
@@ -21,4 +21,42 @@ export const fontFacesCss = css`
       url('/fonts/montserrat/Montserrat-Light.woff') format('woff'),
       url('/fonts/montserrat/Montserrat-Light.ttf') format('truetype');
   }
+
+  /* Montserrat-Bold */
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-display: swap;
+    font-weight: 700;
+    src: local('Montserrat-Bold'),
+      url('/fonts/montserrat/Montserrat-Bold.woff2') format('woff2'),
+      url('/fonts/montserrat/Montserrat-Bold.woff') format('woff'),
+      url('/fonts/montserrat/Montserrat-Bold.ttf') format('truetype');
+  }
+
+  /* Montserrat-ExtraBold */
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-display: swap;
+    font-weight: 800;
+    src: local('Montserrat-ExtraBold'),
+      url('/fonts/montserrat/Montserrat-ExtraBold.woff2') format('woff2'),
+      url('/fonts/montserrat/Montserrat-ExtraBold.woff') format('woff'),
+      url('/fonts/montserrat/Montserrat-ExtraBold.ttf') format('truetype');
+  }
+
+  /* PlayfairDisplay-Medium */
+  @font-face {
+    font-family: 'Playfair Display';
+    font-style: normal;
+    font-display: swap;
+    font-weight: 800;
+    src: local('PlayfairDisplay-Medium'),
+      url('/fonts/playfair-display/PlayfairDisplay-Medium.woff2')
+        format('woff2'),
+      url('/fonts/playfair-display/PlayfairDisplay-Medium.woff') format('woff'),
+      url('/fonts/playfair-display/PlayfairDisplay-Medium.ttf')
+        format('truetype');
+  }
 `;
