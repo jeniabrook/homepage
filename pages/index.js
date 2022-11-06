42eca6d0184f3e095cd1ae5acf6017569aa5a0c8
Jenia Brook <jeniabrook@gmail.com>
2022-11-06 14:56:52 +0200

    Add title

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Intro from '../components/Homepage/Intro';
 
 function Index() {
   return (
     <>
+      <Head>
+        <title>Jenia Brook | Frontend engineer</title>
+      </Head>
       <Intro />
     </>
   );
