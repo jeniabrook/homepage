dab2d4dbbd833a9d76570f1c3df532c282b1aa3a
Jenia Brook <jeniabrook@gmail.com>
2022-11-09 22:20:01 +0200

    Logos scaffolds

--- components/Pages/Home/TechStack/TechStack.js
+++ components/Pages/Home/TechStack/TechStack.js
@@ -1,11 +1,33 @@
+import { css } from '@emotion/react';
 import Container from '../../../Container';
 import SectionHeading from '../../../SectionHeading';
+import Text from '../../../Text';
+import React from './Logos/React';
+
+const technologies = [[React, 'React']];
+
+const logoWrapperCss = css`
+  width: 90px;
+  display: flex;
+`;
 
 function TechStack() {
   return (
     <section>
       <Container>
         <SectionHeading accentColor="orange">Tech Stack</SectionHeading>
+        <div>
+          {technologies.map(([Logo, label]) => {
+            return (
+              <div key={label}>
+                <div css={logoWrapperCss}>
+                  <Logo />
+                </div>
+                <Text>{label}</Text>
+              </div>
+            );
+          })}
+        </div>
       </Container>
     </section>
   );
